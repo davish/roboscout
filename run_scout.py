648ec82e3b18a8cd29c2b6b3a3660646adfa00f2
Davis Haupt <dbh937@gmail.com>
2016-03-21 12:09:12 -0400

    works with command line arguments

--- run_scout.py
+++ run_scout.py
@@ -14,7 +14,7 @@ def print_table(h, d):
 
 if __name__ == '__main__':
     import operator
-    s = scout(getData(sys.argv[1]))
+    s = scout(getData(sys.argv[1] if len(sys.argv) > 1 else 'scoreboard.csv'))
     h = ['#', 'opar', 'oar']
 
     d = []
